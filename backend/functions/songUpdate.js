49ec3715c8e8593de48c4dbee5b9f27ff9b7cd7e
PascalStudent <pswwiersma@st.noorderpoort.nl>
2023-05-17 11:21:23 +0200

    Commit

--- backend/functions/songUpdate.js
+++ backend/functions/songUpdate.js
@@ -2,10 +2,13 @@ const config = require("../config.json");
 const API_URL = config.ICECAST_URL;
 const API_URL_SONGINFO = config.MUZIEKINFO_URL;
 
+// get time and date
+const date = new Date();
+const time = date.getHours() + ":" + date.getMinutes();
+
 let previousData = null; // Track previous data
 
 function startSongUpdateLoop(socket) {
-    // Define an asynchronous function to periodically update the song
     async function updateSong() {
         try {
             const response = await fetch(API_URL);
@@ -25,20 +28,18 @@ function startSongUpdateLoop(socket) {
                 let artist = title[0];
                 let song = title[1];
 
-                // get time and date
-                let date = new Date();
-                let time = date.getHours() + ":" + date.getMinutes();
+                try {
+                    // Remove () from song with year or explicit
+                    if (song.includes("(")) {
+                        song = song.split("(")[0].trim();
+                    }
+                } catch (error) {
+                    console.log("Error: " + error);
+                }
 
                 console.log(
                     "[" + time + "] Nu op de radio: " + artist + " - " + song
                 );
-
-                // try {
-                //     // Remove () from song with year or explicit
-                //     if (song.includes("(")) {
-                //         song = song.split("(")[0].trim();
-                //     }
-                // } catch (error) {}
 
                 getSongInfo(socket, song, artist);
 
@@ -71,13 +72,13 @@ function stopSongUpdateLoop(socket) {
 
 async function getSongInfo(socket, song, artist) {
     try {
-        const response = await fetch(
+        const url =
             API_URL_SONGINFO +
-                "/song?title=" +
-                encodeURIComponent(song) +
-                "&artist=" +
-                encodeURIComponent(artist)
-        );
+            "/song?title=" +
+            encodeURIComponent(song) +
+            "&artist=" +
+            encodeURIComponent(artist);
+        const response = await fetch(url);
         const data = await response.json();
 
         if (data.found == false) {
@@ -85,11 +86,13 @@ async function getSongInfo(socket, song, artist) {
                 "album",
                 "https://live.noordkopcentraal.nl/img/NKC-Logo.png"
             );
+            console.log("Geen album gevonden");
         } else {
             socket.emit("album", data.result.covers.medium);
+            console.log("Album gevonden: " + data.result.covers.medium);
         }
     } catch (error) {
-        console.log(error);
+        console.log("Kan album niet ophalen: " + error);
     }
 }
 
